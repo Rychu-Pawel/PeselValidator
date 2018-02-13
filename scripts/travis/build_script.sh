826acceb2b87a817a203ec30464fa3e6eab6c43a
Rychu <pawel.rychlicki@gmail.com>
2018-02-13 16:34:36 +0100

    Fixed spaces in build_script.sh

--- scripts/travis/build_script.sh
+++ scripts/travis/build_script.sh
@@ -11,6 +11,6 @@ fi
 dotnet build -c Release ./PeselValidator.sln
 dotnet test ./tests/PeselValidatorTests/PeselValidatorTests.csproj --logger:trx
 
-if [ $TRAVIS_PULL_REQUEST = "true" ] || [$TRAVIS_BRANCH = "develop"] || [$TRAVIS_BRANCH = "master"]; then
+if [ $TRAVIS_PULL_REQUEST = "true" ] || [ $TRAVIS_BRANCH = "develop" ] || [ $TRAVIS_BRANCH = "master" ]; then
   mono ./tools/sonar-scanner-msbuild/SonarQube.Scanner.MSBuild.exe end /d:sonar.login=${SONAR_TOKEN}
 fi
