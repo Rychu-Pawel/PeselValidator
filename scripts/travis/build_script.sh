23f4eb4a1a04a040e39f0b7f3231188122451ac7
pawel.rychlicki@gmail.com <pawel.rychlicki@gmail.com>
2018-02-17 20:05:18 +0100

    Making sonar scan develop

--- scripts/travis/build_script.sh
+++ scripts/travis/build_script.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-mono ./tools/sonar-scanner-msbuild/SonarQube.Scanner.MSBuild.exe begin /d:sonar.organization="rychu-pawel-github" /k:PeselValidator /d:sonar.login=${SONAR_TOKEN} /d:sonar.host.url="https://sonarcloud.io" /d:sonar.cs.vstest.reportsPaths="**/*.trx"
+mono ./tools/sonar-scanner-msbuild/SonarQube.Scanner.MSBuild.exe begin /d:sonar.organization="rychu-pawel-github" /k:PeselValidator /d:sonar.login=${SONAR_TOKEN} /d:sonar.host.url="https://sonarcloud.io" /d:sonar.cs.vstest.reportsPaths="**/*.trx" /d:sonar.branch.name="${$TRAVIS_BRANCH}" /d:sonar.branch.target="master"
 
 dotnet build -c Release ./PeselValidator.sln
 dotnet test ./tests/PeselValidatorTests/PeselValidatorTests.csproj --logger:trx
