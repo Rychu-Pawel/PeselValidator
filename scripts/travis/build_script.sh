5fc035d093c01ff20265e3f0e74f614edbec2073
pawel.rychlicki@gmail.com <pawel.rychlicki@gmail.com>
2018-02-17 19:10:28 +0100

    Trying to fix bash condition problem in build_script

--- scripts/travis/build_script.sh
+++ scripts/travis/build_script.sh
@@ -1,16 +1,11 @@
 #!/bin/sh
 
-echo $TRAVIS_PULL_REQUEST_BRANCH
-  echo $TRAVIS_BRANCH
-  echo $TRAVIS_REPO_SLUG
-  echo $TRAVIS_PULL_REQUEST
-
-if [ -v TRAVIS_PULL_REQUEST_BRANCH ]; then
+if [[ -v TRAVIS_PULL_REQUEST_BRANCH ]]; then
   echo "STARTING SONAR IN PULL REQUEST MODE"
-  mono ./tools/sonar-scanner-msbuild/SonarQube.Scanner.MSBuild.exe begin /d:sonar.organization="rychu-pawel-github" /k:PeselValidator /d:sonar.login=${SONAR_TOKEN} /d:sonar.host.url="https://sonarcloud.io" /d:sonar.cs.vstest.reportsPaths="**/*.trx" /d:sonar.branch.name=${TRAVIS_PULL_REQUEST_BRANCH} /d:sonar.branch.target=${TRAVIS_BRANCH} /d:sonar.analysis.mode=preview /d:sonar.verbose=true /d:sonar.github.oauth=${GitHubPullRequestToken} /d:sonar.github.repository=${TRAVIS_REPO_SLUG} /d:sonar.github.pullRequest=${TRAVIS_PULL_REQUEST} /d:sonar.pullrequest.endpoint="https://api.github.com" /d:sonar.pullrequest.repository="Rychu-Pawel/PeselValidator"
+  mono ./tools/sonar-scanner-msbuild/SonarQube.Scanner.MSBuild.exe begin /d:sonar.organization="rychu-pawel-github" /k:PeselValidator /d:sonar.login=${SONAR_TOKEN} /d:sonar.host.url="https://sonarcloud.io" /d:sonar.cs.vstest.reportsPaths="**/*.trx" /d:sonar.branch.name=${TRAVIS_PULL_REQUEST_BRANCH} /d:sonar.branch.target=${TRAVIS_BRANCH} /d:sonar.analysis.mode=preview /d:sonar.verbose=true /d:sonar.github.oauth=${GitHubPullRequestToken} /d:sonar.github.repository=${TRAVIS_REPO_SLUG} /d:sonar.github.pullRequest=${TRAVIS_PULL_REQUEST} /d:sonar.pullrequest.endpoint="https://api.github.com" /d:sonar.pullrequest.repository=${TRAVIS_PULL_REQUEST_BRANCH}
 elif [ $TRAVIS_BRANCH = "develop" ]; then
   echo "STARTING SONAR IN DEVELOP MODE"
-  mono ./tools/sonar-scanner-msbuild/SonarQube.Scanner.MSBuild.exe begin /d:sonar.organization="rychu-pawel-github" /k:PeselValidator /d:sonar.login=${SONAR_TOKEN} /d:sonar.host.url="https://sonarcloud.io" /d:sonar.cs.vstest.reportsPaths="**/*.trx" /d:sonar.branch.name=${TRAVIS_PULL_REQUEST_BRANCH} /d:sonar.branch.target="master"
+  mono ./tools/sonar-scanner-msbuild/SonarQube.Scanner.MSBuild.exe begin /d:sonar.organization="rychu-pawel-github" /k:PeselValidator /d:sonar.login=${SONAR_TOKEN} /d:sonar.host.url="https://sonarcloud.io" /d:sonar.cs.vstest.reportsPaths="**/*.trx" /d:sonar.branch.name=${$TRAVIS_BRANCH} /d:sonar.branch.target="master"
 elif [ $TRAVIS_BRANCH = "master" ]; then
   echo "STARTING SONAR IN MASTER MODE"
   mono ./tools/sonar-scanner-msbuild/SonarQube.Scanner.MSBuild.exe begin /d:sonar.organization="rychu-pawel-github" /k:PeselValidator /d:sonar.login=${SONAR_TOKEN} /d:sonar.host.url="https://sonarcloud.io" /d:sonar.cs.vstest.reportsPaths="**/*.trx"
