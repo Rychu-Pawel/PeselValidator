f565fbf912e8a9ff19b81838d2c87e252c8ad56b
pawel.rychlicki@gmail.com <pawel.rychlicki@gmail.com>
2018-02-18 20:23:24 +0100

    Changed branch name in build_script for sonar develop mode

--- scripts/travis/build_script.sh
+++ scripts/travis/build_script.sh
@@ -5,7 +5,7 @@ if [[ $TRAVIS_PULL_REQUEST != "false" ]]; then
   mono ./tools/sonar-scanner-msbuild/SonarQube.Scanner.MSBuild.exe begin /d:sonar.organization="rychu-pawel-github" /k:PeselValidator /d:sonar.login=${SONAR_TOKEN} /d:sonar.host.url="https://sonarcloud.io" /d:sonar.cs.vstest.reportsPaths="**/*.trx" /d:sonar.analysis.mode=preview /d:sonar.github.oauth="${GitHubPullRequestToken}" /d:sonar.github.repository="${TRAVIS_REPO_SLUG}" /d:sonar.github.pullRequest=${TRAVIS_PULL_REQUEST}
 elif [[ $TRAVIS_BRANCH = "develop" ]]; then
   echo "STARTING SONAR IN DEVELOP MODE"
-  mono ./tools/sonar-scanner-msbuild/SonarQube.Scanner.MSBuild.exe begin /d:sonar.organization="rychu-pawel-github" /k:PeselValidator /d:sonar.login=${SONAR_TOKEN} /d:sonar.host.url="https://sonarcloud.io" /d:sonar.cs.vstest.reportsPaths="**/*.trx" /d:sonar.branch.name="${TRAVIS_BRANCH}" /d:sonar.branch.target="master"
+  mono ./tools/sonar-scanner-msbuild/SonarQube.Scanner.MSBuild.exe begin /d:sonar.organization="rychu-pawel-github" /k:PeselValidator /d:sonar.login=${SONAR_TOKEN} /d:sonar.host.url="https://sonarcloud.io" /d:sonar.cs.vstest.reportsPaths="**/*.trx" /d:sonar.branch.name="develop" /d:sonar.branch.target="master"
 elif [[ $TRAVIS_BRANCH = "master" ]]; then
   echo "STARTING SONAR IN MASTER MODE"
   mono ./tools/sonar-scanner-msbuild/SonarQube.Scanner.MSBuild.exe begin /d:sonar.organization="rychu-pawel-github" /k:PeselValidator /d:sonar.login=${SONAR_TOKEN} /d:sonar.host.url="https://sonarcloud.io" /d:sonar.cs.vstest.reportsPaths="**/*.trx"
