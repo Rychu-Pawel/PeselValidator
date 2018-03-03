18c714f8254e5c7afb528eed7cd4ef330360875a
pawel.rychlicki@gmail.com <pawel.rychlicki@gmail.com>
2018-03-03 17:19:28 +0100

    Removed sonar trap

--- src/PeselValidator/PeselValidator.cs
+++ src/PeselValidator/PeselValidator.cs
@@ -9,8 +9,6 @@ namespace Rychusoft.Validators
 
         public static bool IsValid(string pesel)
         {
-            var trapForSonar = true;
-
             if (IsEmptyOrNotProperLength(pesel))
                 return false;
 
